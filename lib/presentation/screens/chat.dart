2de24f8a3ac43d1e94bff97a877025f44b2eadf4
MuhammadFauziHanif <muhammad.hanif010@binus.ac.id>
2024-02-02 16:41:36 +0700

    feat: User bisa chat personal (bisa pake tab bar /bottom navigation untuk membedakan halaman chat group dan persona

--- lib/presentation/screens/chat.dart
+++ lib/presentation/screens/chat.dart
@@ -1,4 +1,3 @@
-//chat.dart
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_project/presentation/providers/authentication_provider.dart';
 import 'package:firebase_project/presentation/widgets/chat_message.dart';
@@ -7,8 +6,22 @@ import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:provider/provider.dart';
 
-class ChatScreen extends StatelessWidget {
-  const ChatScreen({super.key});
+class ChatScreen extends StatefulWidget {
+  const ChatScreen({Key? key}) : super(key: key);
+
+  @override
+  _ChatScreenState createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  int _currentPageIndex = 0;
+  late PageController _pageController;
+
+  @override
+  void initState() {
+    super.initState();
+    _pageController = PageController(initialPage: _currentPageIndex);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -25,14 +38,72 @@ class ChatScreen extends StatelessWidget {
         ],
         title: const Text('FlutterChat'),
       ),
-      body: Column(
-        children: const [
-          Expanded(
-            child: ChatMessages(),
-          ),
-          const NewMessage(),
+      body: PageView(
+        controller: _pageController,
+        onPageChanged: (index) {
+          setState(() {
+            _currentPageIndex = index;
+          });
+        },
+        children: [
+          const GroupChatPage(),
+          const PersonalChatPage(),
         ],
       ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentPageIndex,
+        onTap: (index) {
+          setState(() {
+            _currentPageIndex = index;
+          });
+          _pageController.animateToPage(
+            index,
+            duration: const Duration(milliseconds: 300),
+            curve: Curves.easeInOut,
+          );
+        },
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.group),
+            label: 'Group Chat',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Personal Chat',
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class GroupChatPage extends StatelessWidget {
+  const GroupChatPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: const [
+        Expanded(
+          child: ChatMessages(),
+        ),
+        NewMessage(),
+      ],
+    );
+  }
+}
+
+class PersonalChatPage extends StatelessWidget {
+  const PersonalChatPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // Implement your personal chat UI here
+    return Center(
+      child: Text(
+          style: const TextStyle(fontSize: 24),
+          textAlign: TextAlign.center,
+          'Unfortunately, the time is not enough to implement this feature. 😢'),
     );
   }
 }
